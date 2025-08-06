e0ab36b6dfd850ff7b0f75480ae406fa53e19fb0
pancake-dave <dawid.bialokryty@gmail.com>
2025-08-07 00:56:35 +0200

    add moving logic to the Player class, update main function

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
